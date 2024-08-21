e263424bcf3d7bf00899cf73a2e42fb59b43af7b
Denilson Guedes <denilson.guedes@xipefo.com>
2024-08-21 13:03:40 +0200

    finished: tabs filter

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,38 @@
 import { Navbar } from "@/components/Navbar";
-import Image from "next/image";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 export default function Home() {
   return (
     <main className="w-full h-full min-h-screen bg-zinc-100">
       <Navbar />
+      <section id="content" className="flex justify-center px-6 py-8">
+      <div id="products" className="max-w-5xl w-full flex justify-between items-center">
+        <div className="w-full">
+        <Tabs defaultValue="all" className="">
+          <div className="w-full flex justify-between">
+          <TabsList className="bg-transparent gap-10">
+            <TabsTrigger value="all" className="tab-button py-1 px-0">
+              TODOS OS PRODUTOS
+            </TabsTrigger>
+            <TabsTrigger value="t-shirts" className="tab-button py-1 px-0">
+              CAMISETAS
+            </TabsTrigger>
+            <TabsTrigger value="sneakers" className="tab-button py-1 px-0">
+              SAPATILHAS
+            </TabsTrigger>
+          </TabsList>
+        <div className="flex-col">
+          Organizar
+          <p>Pagination</p>
+        </div>
+          </div>
+          <TabsContent value="all">Make changes to your account here.</TabsContent>
+          <TabsContent value="shirts">Change your password here.</TabsContent>
+          <TabsContent value="sneakers">Change your password here.</TabsContent>
+        </Tabs>
+      </div>
+        </div>
+      </section>
     </main>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link'
 import React from 'react'
-import { Badge } from './ui/badge'
 import { Input } from './ui/input'
 import { Button } from './ui/button'
 import Logo from '../app/logo.png'
@@ -18,10 +17,7 @@ export const Navbar: React.FC = () => {
                 </div>
                 <Link href="/cart" className='flex items-end relative'>
                     <i className="ri-shopping-bag-3-line text-[22px] text-muted-foreground"></i>
-                    {/* <sub><Badge variant="destructive" className='w-[20px] h-[20px]'>2</Badge></sub> */}
                     <div className="absolute right-[-8px] w-[16px] h-[16px] flex justify-center items-center rounded-full bg-red-500"><small className='text-white'>2</small></div>
-                    {/* <sub className='grid place-items-center text-white bg-red-600 p-1 rounded-full'>2</sub> */}
-                    {/* <i className="ri-shopping-bag-line text-[14px]"></i> */}
                 </Link>
             </div>
         </div>
